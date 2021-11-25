d2a6a1840f4a842dd91f268ba7e666493b9c807f
vrudas <vrudas4work@gmail.com>
2021-11-25 23:13:17 +0200

    Read user credentials from console

--- unit-testing-example/src/main/java/io/testing/unit/Main.java
+++ unit-testing-example/src/main/java/io/testing/unit/Main.java
@@ -3,16 +3,26 @@ package io.testing.unit;
 import io.testing.unit.domain.User;
 import io.testing.unit.domain.UserService;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         var userService = new UserService();
 
-        User user = userService.createUser(
-            "user@email.com",
-            "password"
-        );
+        try (var scanner = new Scanner(System.in)) {
+            System.out.println("User email:");
+            String email = scanner.nextLine();
 
-        System.out.println("user = " + user);
+            System.out.println("User password:");
+            String password = scanner.nextLine();
+
+            User user = userService.createUser(
+                email,
+                password
+            );
+
+            System.out.println("user = " + user);
+        }
     }
 }
