369cd0ba2c9c05fa0c377719aa4cc732d0d91e29
vrudas <vrudas4work@gmail.com>
2021-11-29 22:11:22 +0200

    Added article to LeftPad problem

--- tdd-example/src/main/java/io/testing/tdd/LeftPad.java
+++ tdd-example/src/main/java/io/testing/tdd/LeftPad.java
@@ -1,5 +1,10 @@
 package io.testing.tdd;
 
+/**
+ * Problem described in article
+ *
+ * <a href="https://medium.com/@laravelfeed/how-one-developer-just-broke-node-babel-and-thousands-of-projects-in-11-lines-of-javascript-ddb6844772a8">How one developer just broke Node, Babel and thousands of projects in 11 lines of JavaScript</a>
+ */
 public class LeftPad {
     public static String leftPad(String str, int spacesCount) {
         if (str == null) {
